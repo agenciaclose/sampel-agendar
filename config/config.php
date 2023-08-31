ca6c563f97d773473a5e4eda47cafd014a4d3a03
Rafael Luiz <rafael@agenciaclose.com.br>
2023-08-31 10:04:06 -0300

    Sistema quase pronto

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+    define('DOMAIN', 'https://www.sampel.com.br/evento');
+    define('PATH', 'https://www.sampel.com.br/evento');
+    define('NAME', 'Sampel - Evento');
+    define('PRODUCTION', false);
+
+    define('DOMAIN_MAIN', 'https://www.sampel.com.br/evento');
+    define('PATH_MAIN', 'https://www.sampel.com.br/evento');
+
+    // CONFIGURAÇÕES DO BANCO ########################
+    define('HOST_MAIN', '177.234.145.178');
+    define('USER_MAIN', 'sampel_evento');
+    define('PASS_MAIN', 'oG7ElprDRWDiRWNAEL');
+    define('DBSA_MAIN', 'sampel_evento');
