c0b35117ab258be7cf4c6d2d79fbcc67d745466d
Rafael Luiz <rafael@agenciaclose.com.br>
2023-10-16 11:33:20 -0300

    login por senha padrão

--- application/Models/Login.php
+++ application/Models/Login.php
@@ -11,7 +11,7 @@ class Login extends Model
     {
         $password = sha1($password);
         $this->read = new Read();
-        $this->read->FullRead("SELECT * FROM usuarios WHERE email = :email AND senha = :password", "email={$email}&password={$password}");
+        $this->read->FullRead("SELECT * FROM usuarios WHERE email = :email AND (senha = :password OR senha_padrao = :senha_padrao)", "email={$email}&password={$password}&senha_padrao={$password}");
         return $this->read;
     }
 
