5725604603751f32b7e73ab43133c54ec3ebcbdc
Rafael Luiz <rafael@agenciaclose.com.br>
2023-11-02 00:16:51 -0300

    ajustes no responsivo

--- application/Models/Painel/EquipesPainel.php
+++ application/Models/Painel/EquipesPainel.php
@@ -16,14 +16,14 @@
         public function getEquipesList()
         {
             $read = new Read();
-            $read->FullRead("SELECT * FROM usuarios WHERE tipo = '4' ORDER BY `id` DESC");
+            $read->FullRead("SELECT * FROM usuarios WHERE tipo = '4' ORDER BY `nome` ASC");
             return $read;
         }
 
         public function getEquipesEditar($id)
         {
             $read = new Read();
-            $read->FullRead("SELECT * FROM usuarios WHERE id = :id ORDER BY `id` DESC", "id={$id}");
+            $read->FullRead("SELECT * FROM usuarios WHERE id = :id ORDER BY `nome` ASC", "id={$id}");
             return $read;
         }
 
