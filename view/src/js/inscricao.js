f55a1dfa27ea72a72fd9d61194ae77b85fb23bf9
Rafael Luiz <rafael@agenciaclose.com.br>
2023-11-30 14:58:14 -0300

    Salvar imagem da inscricao

--- view/src/js/inscricao.js
+++ view/src/js/inscricao.js
@@ -153,3 +153,23 @@ $(".cpf_autocomplete").blur(function() {
     });
 
 });
+
+//Creating dynamic link that automatically click
+$(document).ready(function() {
+    var element = $("#inscricao_sucesso"); // global variable
+    var getCanvas; // global variable
+    var newData;
+
+    $("#salvar_inscricao").on('click', function() {
+        html2canvas(element,{
+            onrendered: function(canvas) {
+                getCanvas = canvas;
+                var imgageData = getCanvas.toDataURL("image/png");
+                var a = document.createElement("a");
+                a.href = imgageData; //Image Base64 Goes here
+                a.download = "inscricao.png"; //File name Here
+                a.click(); //Downloaded file
+            }
+        });
+    });
+});
