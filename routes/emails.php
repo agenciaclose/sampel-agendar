8a661fe5f0e53d9464e3e672d15b0209334f0d5d
Rafael Luiz <rafael@agenciaclose.com.br>
2023-12-08 11:44:31 -0300

    Ajustes gerais

--- routes/emails.php
+++ routes/emails.php
@@ -1,6 +1,7 @@
 <?php
 
 // SEND EMAIL
+$router->namespace("Agencia\Close\Controllers\Site\Home");
 $router->get("/visita/emailEquipeTemplate/{visita_id}", "HomeController:emailEquipeTemplate");
 $router->get("/visita/sendEmailEquipe/{visita_id}", "HomeController:sendEmailEquipe");
 $router->get("/visita/sendEmailEstatisticas/{visita_id}", "HomeController:sendEmailEstatisticas");
