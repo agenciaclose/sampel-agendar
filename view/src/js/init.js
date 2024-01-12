d5d9365f6da4448838060aa7f801df0ab51543c8
Rafael Luiz <rafael@agenciaclose.com.br>
2024-01-12 16:15:55 -0300

    Ajustes na lista de inscritos

--- view/src/js/init.js
+++ view/src/js/init.js
@@ -15,6 +15,7 @@ import './app';
 import DataTable from 'datatables.net-dt';
  
 let table = new DataTable('#tabela_dinamica',  {
+    dom: '<"top"fi>rt<"bottom"lp><"clear">',
     order: [[0, 'asc']],
     language: {
         url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json',
@@ -22,6 +23,7 @@ let table = new DataTable('#tabela_dinamica',  {
 });
 
 let table_concluida = new DataTable('#tabela_dinamica_concluida',  {
+    dom: '<"top"fi>rt<"bottom"lp><"clear">',
     order: [[0, 'desc']],
     language: {
         url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json',
@@ -29,6 +31,7 @@ let table_concluida = new DataTable('#tabela_dinamica_concluida',  {
 });
 
 let table_palestra = new DataTable('#tabela_dinamica_palestra',  {
+    dom: '<"top"fi>rt<"bottom"lp><"clear">',
     order: [[1, 'desc']],
     language: {
         url: '//cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json',
