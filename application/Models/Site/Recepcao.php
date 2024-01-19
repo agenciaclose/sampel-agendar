cf2fad83a5c681e61006ac4d14a4c8a6062e4477
Rafael Luiz <rafael@agenciaclose.com.br>
2024-01-19 14:49:20 -0300

    Ajustes no cadastro da visita

--- application/Models/Site/Recepcao.php
+++ application/Models/Site/Recepcao.php
@@ -11,8 +11,14 @@ class Recepcao extends Model
 {
     public function confirmarPresenca($params)
     {
+        if (strlen($params['codigo']) > 8) {
+            $codigo = " AND `cpf` = '".$params['codigo']."'";
+        } else {
+            $codigo = " AND `codigo` = '".$params['codigo']."'";
+        }
+
         $read = new Read();
-        $read->FullRead("UPDATE `visitas_inscricoes` SET `presenca` = 'Sim' WHERE `id_visita` = '".$params['id_visita']."' AND `codigo` = '".$params['codigo']."'");
+        $read->FullRead("UPDATE `visitas_inscricoes` SET `presenca` = 'Sim' WHERE `id_visita` = '".$params['id_visita']."' $codigo");
         return $read;
     }
 
