f00efdd6fa592d0986e88507b0c14090de43c0f5
Rafael Luiz <rafael@agenciaclose.com.br>
2024-01-29 10:31:02 -0300

    Ajustes nos relatorios de visitas

--- application/Controllers/Site/Relatorios/RelatoriosController.php
+++ application/Controllers/Site/Relatorios/RelatoriosController.php
@@ -23,6 +23,9 @@ class RelatoriosController extends Controller
         $total_setor = new Relatorios();
         $total_setor = $total_setor->getTotalSetor()->getResult();
 
+        $total_setor_confirmados = new Relatorios();
+        $total_setor_confirmados = $total_setor_confirmados->getTotalSetorConfirmados()->getResult();
+
         $total_cidade = new Relatorios();
         $total_cidade = $total_cidade->getTotalCidade()->getResult();
 
@@ -35,6 +38,7 @@ class RelatoriosController extends Controller
             'numeros' => $numeros,
             'total' => $total,
             'total_setor' => $total_setor,
+            'total_setor_confirmados' => $total_setor_confirmados,
             'total_cidade' => $total_cidade,
             'total_equipe' => $total_equipe
         ]);
--- application/Models/Site/Relatorios.php
+++ application/Models/Site/Relatorios.php
@@ -39,6 +39,16 @@ class Relatorios extends Model
         return $read;
     }
 
+    public function getTotalSetorConfirmados(): Read
+    {
+        $read = new Read();
+        $read->FullRead("SELECT vi.id, vi.setor, COUNT(vi.id) AS total FROM `visitas_inscricoes` AS vi
+                        INNER JOIN visitas AS v ON v.id = vi.id_visita
+                        WHERE v.status_visita = 'Concluido' AND  vi.setor <> '' AND vi.presenca = 'Sim'
+                        GROUP BY vi.setor ORDER BY total DESC LIMIT 10");
+        return $read;
+    }
+
     public function getTotalCidade(): Read
     {
         $read = new Read();
