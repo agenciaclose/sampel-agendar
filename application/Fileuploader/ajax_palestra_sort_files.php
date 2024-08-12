23a86b2c886f9ba351265dbbb23215fd9e73b8d2
Rafael Luiz <rafael@agenciaclose.com.br>
2024-08-12 12:21:20 -0300

    Nova Home

--- application/Fileuploader/ajax_palestra_sort_files.php
+++ application/Fileuploader/ajax_palestra_sort_files.php
@@ -4,8 +4,6 @@
     $list = isset($_POST['_list']) ? json_decode($_POST['_list'], true) : null;
    
 	foreach ($list as $key => $item){
-
-		$sql = $db->prepare("UPDATE `palestras_imagens` SET `order` = '".$item['index']."' WHERE `id_palestra` = '".$_GET['id_paletra']."' AND nome = '".$item['name']."'");
+		$sql = $db->prepare("UPDATE `palestras_imagens` SET `order` = '".$item['index']."' WHERE `id_palestra` = '".$_GET['id_palestra']."' AND nome = '".$item['name']."'");
 		$sql->execute();
-		
 	}
