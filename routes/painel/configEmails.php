00c7b4438c3e9db0326d2e58f0c2d0d7490e0b29
Rafael Luiz <rafael@agenciaclose.com.br>
2025-05-26 12:54:46 -0300

    Ajustes

--- routes/painel/configEmails.php
+++ routes/painel/configEmails.php
@@ -2,5 +2,5 @@
 
 // PAINEL CONFIGURAÇÃO DE EMAILS
 $router->namespace("Agencia\Close\Controllers\Painel\Config");
-$router->get("/painel/configuracoes/emails", "ConfigEmailsController:index");
+$router->get("/painel/configuracoes/emails-config", "ConfigEmailsController:index");
 $router->post("/painel/configuracoes/emails/salvar", "ConfigEmailsController:salvar"); 
