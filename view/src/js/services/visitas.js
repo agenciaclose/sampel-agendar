43e894f7e099da23cf6e6c5fc7422fc2c054f32d
Rafael Luiz <rafael@agenciaclose.com.br>
2026-03-14 10:42:05 -0300

    Ajustes

--- view/src/js/services/visitas.js
+++ view/src/js/services/visitas.js
@@ -130,47 +130,37 @@ $('#tabela_dinamica').on('click', '.btn-excluir-inscricao', function (e) {
 
 // GERA QRCODE DA PALESTRA
 function qrcodeGenVisitas(id_visita) {
-    // var DOMAIN = $('body').data('domain');
-    
-    // $.ajax({
-    //     type: "POST", 
-    //     async: true, 
-    //     data: { 
-    //             "frame_name": "bottom-frame",
-    //             "qr_code_text": DOMAIN + '/visita/inscricao/'+id_visita+'?a=qr',
-    //             "image_format": "SVG",
-    //             "frame_color": "#246CB1",
-    //             "frame_text_color": "#ffffff",
-    //             "frame_icon_name": "mobile",
-    //             "frame_text": "INSCRIÇÃO",
-    //             "marker_left_template": "version13",
-    //             "marker_right_template": "version13",
-    //             "marker_bottom_template": "version13"
-    //         },
-    //     url: 'https://api.qr-code-generator.com/v1/create?access-token=pec_cfJ6r3zAxzXl-jCpj8hEj1_R9-9PlkdC8d_pf0Vjpls62BT9NxSQtnySGh43',
-    //     success: function (qrcode) {
-    //         qrcode = (new XMLSerializer()).serializeToString(qrcode);
-    //         qrcodeSaveVisitas(id_visita, qrcode);
-    //     }
-    // });
-
     var DOMAIN = $('body').data('domain');
-    var url = DOMAIN + '/visita/inscricao/' + id_visita + '?a=qr';
-    var apiUrl = "https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=" + encodeURIComponent(url) + "&format=svg";
-
+    
     $.ajax({
-        type: "GET",
-        url: apiUrl,
-        dataType: "text",
-        success: function (svgString) {
-            if (svgString) {
-                qrcodeSaveVisitas(id_visita, svgString);
-            }
-        },
-        error: function () {
-            console.error('Erro ao gerar QR Code');
+        type: "POST", 
+        async: true, 
+        data: { 
+                "frame_name": "bottom-frame",
+                "qr_code_text": DOMAIN + '/visita/inscricao/'+id_visita+'?a=qr',
+                "image_format": "SVG",
+                "frame_color": "#246CB1",
+                "frame_text_color": "#ffffff",
+                "frame_icon_name": "mobile",
+                "frame_text": "INSCRIÇÃO",
+                "marker_left_template": "version13",
+                "marker_right_template": "version13",
+                "marker_bottom_template": "version13"
+            },
+        url: 'https://api.qr-code-generator.com/v1/create?access-token=pec_cfJ6r3zAxzXl-jCpj8hEj1_R9-9PlkdC8d_pf0Vjpls62BT9NxSQtnySGh43',
+        success: function (qrcode) {
+            qrcode = (new XMLSerializer()).serializeToString(qrcode);
+            qrcodeSaveVisitas(id_visita, qrcode);
         }
     });
+
+    var DOMAIN = $('body').data('domain');
+
+    var url = DOMAIN + '/painel/visita/feedback/' + id_user + '/' + id_visita;
+
+    var qrcode = "https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=" + encodeURIComponent(url);
+
+    qrcodeSave(id_visita, id_user, qrcode);
 }
 
 function qrcodeSaveVisitas (id_visita, qrcode){
@@ -198,20 +188,26 @@ function qrcodeSaveVisitas (id_visita, qrcode){
 // GERA QRCODE DO FEEDBACK
 function qrcodeGenFeedback(id_visita) {
     var DOMAIN = $('body').data('domain');
-    var url = DOMAIN + '/visita/feedback/' + id_visita;
-    var apiUrl = "https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=" + encodeURIComponent(url) + "&format=svg";
-
+    
     $.ajax({
-        type: "GET",
-        url: apiUrl,
-        dataType: "text",
-        success: function (svgString) {
-            if (svgString) {
-                qrcodeSaveFeedback(id_visita, svgString);
-            }
-        },
-        error: function () {
-            console.error('Erro ao gerar QR Code do feedback');
+        type: "POST", 
+        async: true, 
+        data: { 
+                "frame_name": "bottom-frame",
+                "qr_code_text": DOMAIN + '/visita/feedback/'+id_visita,
+                "image_format": "SVG",
+                "frame_color": "#246CB1",
+                "frame_text_color": "#ffffff",
+                "frame_icon_name": "mobile",
+                "frame_text": "FEEDBACK",
+                "marker_left_template": "version13",
+                "marker_right_template": "version13",
+                "marker_bottom_template": "version13"
+            },
+        url: 'https://api.qr-code-generator.com/v1/create?access-token=pec_cfJ6r3zAxzXl-jCpj8hEj1_R9-9PlkdC8d_pf0Vjpls62BT9NxSQtnySGh43',
+        success: function (qrcode) {
+            qrcode = (new XMLSerializer()).serializeToString(qrcode);
+            qrcodeSaveFeedback(id_visita, qrcode);
         }
     });
 }
